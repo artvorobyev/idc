ae4f6f3b669529ac569350b45d836169379af981
Artemy Vorobyev <79062272107@yandex.ru>
2019-09-22 18:28:36 +0300

    add typograf

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './img/logo.svg';
-import { Input } from 'reactstrap';
-import queryString from 'query-string';
-import moment from 'moment';
+import React from "react";
+import logo from "./img/logo.svg";
+import { Input } from "reactstrap";
+import queryString from "query-string";
+import moment from "moment";
 
-import Banner from './components/Banner/';
-import Button from './components/Button/';
-import Select from './components/Select/';
-import Upload from './components/Upload/';
+import Banner from "./components/Banner/";
+import Button from "./components/Button/";
+import Select from "./components/Select/";
+import Upload from "./components/Upload/";
 
-import './App.scss';
+import "./App.scss";
 
 class App extends React.Component {
   constructor(props) {
@@ -17,12 +17,12 @@ class App extends React.Component {
     var zoom = this.calculateZoom();
     this.state = {
       zoom: zoom,
-      format: {value: 'slogan', label: 'Slogan'},
-      title: 'Apply now!',
-      description: 'We are waiting for your presentations',
-      role: 'Higher School of Economics',
-      image: 'https://spb.hse.ru/data/2018/12/28/1142999882/IMG_8965.jpg',
-    }
+      format: { value: "slogan", label: "Slogan" },
+      title: "Apply now!",
+      description: "We are waiting for your presentations",
+      role: "Higher School of Economics",
+      image: "https://spb.hse.ru/data/2018/12/28/1142999882/IMG_8965.jpg"
+    };
   }
 
   inputChange(event) {
@@ -34,7 +34,7 @@ class App extends React.Component {
 
   addedImage(file) {
     console.log(file);
-    this.setState({image: file.dataURL});
+    this.setState({ image: file.dataURL });
   }
 
   componentDidMount() {
@@ -42,133 +42,135 @@ class App extends React.Component {
   }
 
   windowResize() {
-    this.setState({zoom: this.calculateZoom()})
+    this.setState({ zoom: this.calculateZoom() });
   }
 
   calculateZoom() {
-    return ( window.innerWidth - 460 ) / 1230;
+    return (window.innerWidth - 460) / 1230;
   }
 
   formSubmit(event) {
     var state = this.state;
     var _self = this;
     event.preventDefault();
-    _self.setState({loading: true});
-    
-    fetch(process.env.REACT_APP_API_URL+'/screenshot',{
+    _self.setState({ loading: true });
+
+    fetch(process.env.REACT_APP_API_URL + "/screenshot", {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json; charset=UTF-8', 
-        'Access-Control-Allow-Origin': '*'
+        "Content-Type": "application/json; charset=UTF-8",
+        "Access-Control-Allow-Origin": "*"
       },
-      mode: 'cors',
+      mode: "cors",
       body: JSON.stringify({
         format: state.format.value,
         title: state.title,
         description: state.description,
         role: state.role,
-        image: state.image,
-      }),
-    }).then((res) => {
-
-      return res.blob();
+        image: state.image
+      })
     })
-    .then((response) => {
-      var link = document.createElement("a");
-      link.href = window.URL.createObjectURL(response);
-      link.download = moment().format('YYYY-MM-DD-HH-mm-ss');
-      link.click();
-      _self.setState({loading: false});
-      console.log('Успех');
-      link.remove();
-    })
-    .catch((error) => {
-      _self.setState({loading: false});
-      console.error('Ошибка:', error);
-      alert('Произошла ошибка. Попробуйте снова.');
-    });
-    
-    
-
-
+      .then(res => {
+        return res.blob();
+      })
+      .then(response => {
+        var link = document.createElement("a");
+        link.href = window.URL.createObjectURL(response);
+        link.download = moment().format("YYYY-MM-DD-HH-mm-ss");
+        link.click();
+        _self.setState({ loading: false });
+        console.log("Успех");
+        link.remove();
+      })
+      .catch(error => {
+        _self.setState({ loading: false });
+        console.error("Ошибка:", error);
+        alert("Произошла ошибка. Попробуйте снова.");
+      });
   }
 
   render() {
     var _self = this;
-    var imageFormats = ['photo','icon','speaker'];
+    var imageFormats = ["photo", "icon", "speaker"];
     var params = queryString.parse(window.location.search);
-    if (params.page === 'banner'){
+    if (params.page === "banner") {
       var banner = Object.assign({}, params);
-      banner.format = {value: params.format};
-      return(
-        <Banner {...banner}/>
-        );
-    }
-
-    else {
+      banner.format = { value: params.format };
+      return <Banner {...banner} />;
+    } else {
       return (
         <div className="App">
-        <div className="App__screen">
-        <div className="App__banner" style={{zoom: _self.state.zoom}}>
-        <Banner
-        imageFormats={imageFormats}
-        {..._self.state}
-        />
-        </div>
-        </div>
-        <div className="App__sidebar">
-        <header className="App__header">
-        <img src={logo} alt="" className="App__logo"/>
-        <h1 className="App__title">inequality<br/> and diversity<br/> conference</h1>
-        </header>
-
-        <form className="App__form" onSubmit={_self.formSubmit.bind(_self)}>
-        <Select
-        parent={_self} 
-        name="format" 
-        placeholder="Format" 
-        options={[
-          { value: 'slogan', label: 'Slogan' },
-          { value: 'photo', label: 'Photo' },
-          { value: 'icon', label: 'Icon'},
-          { value: 'number', label: 'Number'},
-          { value: 'speaker', label: 'Speaker'},
-          ]}
-          />
-          <Input
-          name="title"
-          placeholder="Title" 
-          value={_self.state.title} 
-          className="mt-3"
-          onChange={_self.inputChange.bind(_self)}
-          />
-          <Input
-          name="role"
-          placeholder="Role" 
-          value={_self.state.role} 
-          className={`mt-3 ${ (_self.state.format.value !== 'speaker' ) ? 'd-none' : '' } `}
-          onChange={_self.inputChange.bind(_self)}
-          />
-          <Input 
-          name="description"
-          type="textarea" 
-          value={_self.state.description} 
-          placeholder="Description" 
-          className="mt-3"
-          onChange={_self.inputChange.bind(_self)}
-          />
-
-          <Upload 
-          className={`mt-3 ${ (imageFormats.includes(_self.state.format.value)) ? '' : 'd-none' }`}
-          handler={_self.addedImage.bind(_self)}
-          >Upload Image</Upload>
-
-          <Button loading={_self.state.loading} className="mt-4">Generate</Button>
-
-          </form>
+          <div className="App__screen">
+            <div className="App__banner" style={{ zoom: _self.state.zoom }}>
+              <Banner imageFormats={imageFormats} {..._self.state} />
+            </div>
           </div>
+          <div className="App__sidebar">
+            <header className="App__header">
+              <img src={logo} alt="" className="App__logo" />
+              <h1 className="App__title">
+                inequality
+                <br /> and diversity
+                <br /> conference
+              </h1>
+            </header>
+
+            <form className="App__form" onSubmit={_self.formSubmit.bind(_self)}>
+              <Select
+                parent={_self}
+                name="format"
+                placeholder="Format"
+                options={[
+                  { value: "slogan", label: "Slogan" },
+                  { value: "photo", label: "Photo" },
+                  { value: "icon", label: "Icon" },
+                  { value: "number", label: "Number" },
+                  { value: "speaker", label: "Speaker" }
+                ]}
+              />
+              <Input
+                name="title"
+                placeholder="Title"
+                value={_self.state.title}
+                className="mt-3"
+                onChange={_self.inputChange.bind(_self)}
+              />
+              <Input
+                name="role"
+                placeholder="Role"
+                value={_self.state.role}
+                className={`mt-3 ${
+                  _self.state.format.value !== "speaker" ? "d-none" : ""
+                } `}
+                onChange={_self.inputChange.bind(_self)}
+              />
+              <Input
+                name="description"
+                type="textarea"
+                value={_self.state.description}
+                placeholder="Description"
+                className="mt-3"
+                onChange={_self.inputChange.bind(_self)}
+              />
+
+              <Upload
+                className={`mt-3 ${
+                  imageFormats.includes(_self.state.format.value)
+                    ? ""
+                    : "d-none"
+                }`}
+                handler={_self.addedImage.bind(_self)}
+              >
+                Upload Image
+              </Upload>
+
+              <Button loading={_self.state.loading} className="mt-4">
+                Generate
+              </Button>
+            </form>
           </div>
-          );
+        </div>
+      );
     }
   }
 }
--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,32 +1,33 @@
 import React from "react";
+import Typograf from "typograf";
 import "./style.scss";
 
-
-export default class extends React.Component{
-  constructor(props) {
-    super(props);
-  }
-
-  typograph(string) {
-    return string.split(' ').reduce((response, current)=>{
-      let join = (current.length > 3) ? ' ' : '&nbsp;';
-      return response + current + join;
-    },'').replace(/([^>])\n/g, '$1<br/>').trim();
-  }
-
-  render(){
-    var _self = this;
+export default class extends React.Component {
+  render() {
+    var tp = new Typograf({ locale: ["ru", "en-US"] });
     var props = this.props;
-    var css = (props.format.value === 'photo' && props.image) ? {'backgroundImage': `url("${props.image}")`} : {};
+    var css =
+      props.format.value === "photo" && props.image
+        ? { backgroundImage: `url("${props.image}")` }
+        : {};
     return (
       <div className={`Banner Banner--${props.format.value}`} style={css}>
         <div className="Banner__image">
-          <img src={props.image} alt=""/>
+          <img src={props.image} alt="" />
         </div>
-        <div className="Banner__title">{props.title}</div>
-        <div className="Banner__role">{props.role}</div>
-        <div className="Banner__description" dangerouslySetInnerHTML={{ __html: _self.typograph(props.description) }}></div>
+        <div
+          className="Banner__title"
+          dangerouslySetInnerHTML={{ __html: tp.execute(props.title) }}
+        ></div>
+        <div
+          className="Banner__role"
+          dangerouslySetInnerHTML={{ __html: tp.execute(props.role) }}
+        ></div>
+        <div
+          className="Banner__description"
+          dangerouslySetInnerHTML={{ __html: tp.execute(props.description) }}
+        ></div>
       </div>
-      );
+    );
   }
 }
